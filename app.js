a6aadae5a0c1ae05852af4b6026cd10563e4ae1d
Trần Mạnh Cường <89560029+TranManhCuongQN@users.noreply.github.com>
2023-06-26 22:54:17 +0700

    fix

--- app.js
+++ app.js
@@ -37,6 +37,8 @@ app.use((req, res, next) => {
     "https://hci-tube-fe.vercel.app"
   );
   res.setHeader("Access-Control-Allow-Credentials", "true");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, *");
   next();
 });
 
